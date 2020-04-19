fdff5c95c73a2106712f76a94f5485b687037d4f
Yuliya Mishina <julia.reve19@gmail.com>
2020-04-19 23:17:52 +0300

    Added PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+import ru.netology.domain.Post;
+
+
+public class PostManager {
+        private Post[] posts;
+
+        public Post[] searchWall(int ownerId, String domain, String query, int ownerOnly, int count,int offset) {
+            return null;
+        }
+
+        public void deleteWall(int postId, int ownerId) {
+        }
+}
